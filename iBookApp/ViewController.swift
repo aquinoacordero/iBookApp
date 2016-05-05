8779722ea13d8f7f2a944ecc7a6e86a7ada40e95
oem <oem@Mac-Pro-de-oem.local>
2016-05-05 02:55:03 -0700

    primer commit

--- iBookApp/ViewController.swift
+++ iBookApp/ViewController.swift
@@ -8,18 +8,188 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIGestureRecognizerDelegate{
+    
+    
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    var imgPre="virgen"
+    var img=1
+    var text = " Swift seleccionando una palabra para linkear a un sitio rfrferfe erferfer bbtybyt egtreg rutnui tg gtr grtg rtggunuig rwtgtg nuin nvriunv reujk " as NSString
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        println("AQUI LLEGA")
+        
+        var texto = NSMutableAttributedString(string: text as String)
+        let myCustomAtri = [NSForegroundColorAttributeName: UIColor.blueColor(), NSUnderlineStyleAttributeName: 1]
+        texto.addAttributes(myCustomAtri, range: text.rangeOfString("palabra"))
+        textView.attributedText = texto
+        
+        let tap = UITapGestureRecognizer(target: self, action: Selector("tapTextView:"))
+        tap.delegate = self
+        textView.addGestureRecognizer(tap)
+        
+        var imgName=imgPre+String(img)
+        
+        println(imgName)
+        
+        imagen.image=UIImage(named: imgName)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tapTextView(sender: UITapGestureRecognizer){
+        
+        var substring:String=""
+        var poststring:String=""
+        var palabraSub:String=""
+        var palabraPos:String=""
+        
+        print("Entro\n")
+        
+        let myTextView = sender.view as UITextView
+        let layoutManager = myTextView.layoutManager
+        
+        // location of tap in myTextView coordinates and taking the inset into account
+        var location = sender.locationInView(myTextView)
+        location.x -= myTextView.textContainerInset.left;
+        location.y -= myTextView.textContainerInset.top;
+        
+        // character index at tap location
+        var characterIndex = layoutManager.characterIndexForPoint(location, inTextContainer: myTextView.textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        
+        // if index is valid then do something.
+        if characterIndex < myTextView.textStorage.length {
+            
+            // print the character index
+            print("character index: \(characterIndex) \n")
+            
+            // print the character at the index
+            let myRange = NSRange(location: characterIndex, length: 1)
+            substring = (myTextView.attributedText.string as NSString).substringWithRange(myRange)
+            print("\(substring)\n")
+            
+        }
+        
+        var characterIndexSave=characterIndex
+        var substringSave = substring
+        
+        while substring != " "{
+            
+            let myRange = NSRange(location: characterIndex, length: 1)
+            substring = (myTextView.attributedText.string as NSString).substringWithRange(myRange)
+            palabraSub=palabraSub+substring
+            characterIndex=characterIndex-1
+            //print(palabraSub)
+        }
+        
+        characterIndex=characterIndexSave+1
+        substring=substringSave
+        print("\(substring)\n")
+        
+        while substring != " "{
+            
+            println("\n \(characterIndex)\n")
+            let myRange = NSRange(location: characterIndex, length: 1)
+            substring = (myTextView.attributedText.string as NSString).substringWithRange(myRange)
+            palabraPos=palabraPos+substring
+            characterIndex=characterIndex+1
+            //println(palabraPos)
+        }
+        
+        var palabraSubFin = String(seq: Array(palabraSub).reverse())
+        var palabraPosFin = String(palabraPos)
+        
+        var palabraFin = String(palabraSubFin)+palabraPosFin
+        var palabraFinArray = Array(palabraFin)
+        
+        print("\(palabraFin)a\n")
+        
+        if palabraFin == "" {
+            
+            println("caracter vacio")
 
-
+        }else{
+            
+            palabraFinArray.removeAtIndex(0)
+            palabraFinArray.removeLast()
+            var link = String(seq: palabraFinArray)
+            print("\n\(palabraFinArray)\n")
+            println(link)
+            
+            transition(link)
+        }
+        
+    }
+    
+    func transition(link:String){
+        
+        println("Comunicador \(link)")
+        
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let nextView = storyboard.instantiateViewControllerWithIdentifier("ViewController2") as ViewController2
+        self.presentViewController(nextView, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func tapImagen(sender: AnyObject) {
+        
+        println("tap imagen")
+        
+    }
+    
+    @IBAction func swipeImagen(sender: AnyObject) {
+        
+        if img<4{
+            img++
+        }
+        var imgName=imgPre+String(img)
+        println(imgName)
+        
+        chargueImage(imgName)
+    }
+    
+    @IBAction func swipeImagenRight(sender: AnyObject) {
+        
+        if img>1{
+            img--
+        }
+        var imgName=imgPre+String(img)
+        println(imgName)
+        
+        chargueImage(imgName)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        var transp: ViewController2=segue.destinationViewController as ViewController2
+        
+        var imgName=imgPre+String(img)
+        
+        transp.imgName=imgName
+        
+        println("\n PASANDO DATOS \n")
+    }
+    
+    func chargueImage(img:String){
+        
+        println(img)
+        imagen.image=UIImage(named: img)
+        
+    }
+    
 }
+
+
+
+
 
---
+++ iBookApp/ViewController2.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController2.swift
+//  iBookApp
+//
+//  Created by oem on 04/05/16.
+//  Copyright (c) 2016 oem. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewController2: UIViewController{
+    
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var textViewNote: UITextView!
+    
+    var imgName=""
+    var aZoom = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        /*scrollView.contentSize.height=1000
+        scrollView.contentSize.width=1000*/
+        
+        imagen.image=UIImage(named: imgName)
+        textViewNote.hidden=true
+        
+        println("\(imgName) View2")
+        
+        createButton()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func createButton(){
+        
+        if imgName=="virgen1"{
+            var button1 = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+            button1.frame = CGRectMake(20,20,25,25)
+            button1.addTarget(self, action: "button1Action:", forControlEvents: UIControlEvents.TouchUpInside)
+            
+            var button2 = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+            button2.frame = CGRectMake(220,220,25,25)
+            button2.addTarget(self, action: "button1Action:", forControlEvents: UIControlEvents.TouchUpInside)
+            imagen.addSubview(button1)
+            imagen.addSubview(button2)
+            
+        }
+        else{
+            var button2 = UIButton.buttonWithType(UIButtonType.ContactAdd) as UIButton
+            button2.frame = CGRectMake(0,20,25,25)
+            button2.addTarget(self, action: "button2Action:", forControlEvents: UIControlEvents.TouchUpInside)
+            imagen.addSubview(button2)
+            
+        }
+        
+    }
+    
+    func button1Action(sender: UIButton!){
+        
+        println("BUTTON1")
+        var x=imagen.bounds.height
+        var y=imagen.bounds.width
+        println(x)
+        println(y)
+        
+        textViewNote.layer.cornerRadius = 5
+        textViewNote.alpha = 0.5
+        textViewNote.hidden=false
+        
+        if aZoom==false{
+            self.imagen.transform=CGAffineTransformScale(self.imagen.transform, 2, 2)
+            aZoom=true
+        }else{
+            
+        }
+        
+    }
+    
+    func button2Action(sender: UIButton){
+        println("BUTTON2")
+        
+        var x=imagen.bounds.size
+        println(x)
+        
+        textViewNote.layer.cornerRadius = 5
+        textViewNote.alpha = 0.5
+        textViewNote.hidden=false
+        
+        if aZoom==false{
+            self.imagen.transform=CGAffineTransformScale(CGAffineTransformIdentity, 2, 2)
+            aZoom=true
+        }else{
+            
+        }
+        
+    }
+
+    @IBAction func tapImagenZoom(sender: AnyObject) {
+        
+        if aZoom==true{
+            self.imagen.transform=CGAffineTransformScale(self.imagen.transform, 0.5, 0.5)
+            aZoom=false
+        }else{
+            
+        }
+        
+    }
+    
+}
